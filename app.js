99cb5ab94883de0a060e4b8f00d8c7e5df8214a4
pesilatyuda <pyuda514@gmail.com>
2023-05-10 07:43:06 +0700

    no message

---
+++ app.js
@@ -0,0 +1,5 @@
+// app.js
+
+const biodata = require('./moduleBiodata');
+
+biodata.tampilkanBiodata();
---
+++ moduleBiodata.js
@@ -0,0 +1,12 @@
+// moduleBiodata.js
+
+const tampilkanBiodata = () => {
+  console.log('Nama: Maman Budiman');
+  console.log('Tempat Lahir: Cianjur');
+  console.log('Tanggal Lahir: 8 Agustus 2000');
+  console.log('Alamat: Jl. Neptunus No. 90');
+};
+
+module.exports = {
+  tampilkanBiodata
+};
